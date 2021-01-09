48d7e4fb7a5285a922f1a5b5530b0a9b670af02f
QContinueUm <qcontinueumm@gmail.com>
2021-01-09 14:52:51 +0000

    Add custom overlay style + functions by cell color

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-function showOverlay() {
-    document.getElementById("overlay").className += " show";
+/* Overlay function for teal section */
+
+function showTealOverlay() {
+    document.getElementById("tealoverlay").className += " show";
 }
 
-function hideOverlay() {
-    document.getElementById("overlay").classList.remove("show");
+function hideTealOverlay() {
+    document.getElementById("tealoverlay").classList.remove("show");
+}
+
+/* Overlay function for yellow section */
+
+function showYellowOverlay() {
+    document.getElementById("yellowoverlay").className += " show";
+}
+
+function hideYellowOverlay() {
+    document.getElementById("yellowoverlay").classList.remove("show");
+}
+
+/* Overlay function for purple section */
+
+function showPurpleOverlay() {
+    document.getElementById("purpleoverlay").className += " show";
+}
+
+function hidePurpleOverlay() {
+    document.getElementById("purpleoverlay").classList.remove("show");
+}
+
+/* Overlay function for pink section */
+
+function showPinkOverlay() {
+    document.getElementById("pinkoverlay").className += " show";
+}
+
+function hidePinkOverlay() {
+    document.getElementById("pinkoverlay").classList.remove("show");
 }
